fe2583c07cbdf42002579dc89423780b9301f378
Muhammad Kalmani <muhammadkalmani@Muhammads-MacBook-Pro-3.local>
2024-03-14 11:23:47 +0000

    Initial commit

--- src/main/BakeryDriver.java
+++ src/main/BakeryDriver.java
@@ -1,9 +1,22 @@
+import java.util.ArrayList;
+import bakery.Ingredient;
+
 public class BakeryDriver {
 
     public BakeryDriver() {
     }
 
     public static void main(String[] args)  {
+        Ingredient Flour = new Ingredient("Flour");
+        Ingredient Sugar = new Ingredient("Sugar");
+        Ingredient Eggs = new Ingredient("Eggs");
+        Ingredient Milk = new Ingredient("Milk");
+        ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
+
+        ingredients.add(Flour);
+        ingredients.add(Sugar);
+        ingredients.add(Eggs);
+        ingredients.add(Milk);
     }
 
 }

561dc9a62c179d3c48d4112f5a14b8687a177df2
Muhammad Kalmani <muhammadkalmani@Muhammads-MacBook-Pro-3.local>
2024-03-25 15:09:34 +0300

    Created MagicBakery class: 50 tests passed

--- src/main/BakeryDriver.java
+++ src/main/BakeryDriver.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import bakery.CustomerOrder;
 import bakery.Ingredient;
 import bakery.Layer;
+import bakery.MagicBakery;
 
 public class BakeryDriver {
 
@@ -11,30 +12,7 @@ public class BakeryDriver {
     }
 
     public static void main(String[] args)  {
-        Ingredient flour = new Ingredient("Flour");
-        Ingredient sugar = new Ingredient("Sugar");
-        Ingredient eggs = new Ingredient("Eggs");
-        Ingredient milk = new Ingredient("Milk");
-        Ingredient chocolate = new Ingredient("Chocolate");
-
-        System.out.println(flour);
-        System.out.println(sugar);
-        System.out.println(eggs);
-        System.out.println(milk);
-
-        ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
-        ingredients.add(flour);
-        ingredients.add(sugar);
-        ingredients.add(eggs);
-        ingredients.add(milk);
-
-        ArrayList<Ingredient> garnishes = new ArrayList<Ingredient>();
-        garnishes.add(chocolate);
-
-        Layer layer = new Layer("Layer1", ingredients);
-        System.out.println(layer.getRecipeDescription());
-
-        CustomerOrder order1 = new CustomerOrder("Bob", layer.getRecipe(), garnishes, 1);
+        MagicBakery magicBakery = new MagicBakery(0, null, null);
     }
 
 }
---
+++ src/main/bakery/MagicBakery.java
@@ -0,0 +1,36 @@
+package bakery;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MagicBakery {
+    public MagicBakery(long seed, String ingredientDeckFile, String layerDeckFile){
+
+    }
+
+    public static void main(String[] args){
+        Ingredient flour = new Ingredient("Flour");
+        Ingredient sugar = new Ingredient("Sugar");
+        Ingredient eggs = new Ingredient("Eggs");
+        Ingredient milk = new Ingredient("Milk");
+        Ingredient chocolate = new Ingredient("Chocolate");
+
+        System.out.println(flour);
+        System.out.println(sugar);
+        System.out.println(eggs);
+        System.out.println(milk);
+
+        ArrayList<Ingredient> ingredients = new ArrayList<Ingredient>();
+        ingredients.add(flour);
+        ingredients.add(sugar);
+        ingredients.add(eggs);
+        ingredients.add(milk);
+
+        ArrayList<Ingredient> garnishes = new ArrayList<Ingredient>();
+        garnishes.add(chocolate);
+
+        Layer layer = new Layer("Layer1", ingredients);
+        System.out.println(layer.getRecipeDescription());
+
+        CustomerOrder order1 = new CustomerOrder("Bob", layer.getRecipe(), garnishes, 1);
+    }
+}
